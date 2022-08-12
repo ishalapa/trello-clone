44f18539197461ff769eafb3ccfeee3699884f4d
ishalapa <ihor.shalapa@inventorsoft.co>
2022-08-12 14:29:09 +0300

    add description

--- src/components/Tasks/Task/Task.js
+++ src/components/Tasks/Task/Task.js
@@ -1,9 +1,19 @@
 import React from 'react'
 
-import { Card, Typography } from '@mui/material'
+import { Card, Typography, IconButton } from '@mui/material'
 import { Draggable } from 'react-beautiful-dnd'
 
-const Task = ({ task, index }) => {
+import { RiPencilLine } from 'react-icons/ri'
+import { useDispatch } from 'react-redux'
+import { setCurrentTask } from 'store/slices/currentTaskSlice'
+
+const Task = ({ task, index, handleOpen }) => {
+  const dispatch = useDispatch()
+
+const handleOpenDispatch = () => {
+  dispatch(setCurrentTask(task))
+  handleOpen()
+}
   return (
     <Draggable draggableId={task.id.toString()} index={index}>
       {(provided) => (
@@ -11,9 +21,12 @@ const Task = ({ task, index }) => {
           {...provided.draggableProps}
           {...provided.dragHandleProps}
           ref={provided.innerRef}
-          sx={{ boxShadow: 1, display: 'flex', alignItems: 'center', cursor: 'pointer' }}
+          sx={{ boxShadow: 1, display: 'flex', alignItems: 'center', cursor: 'pointer', position: 'relative' }}
           color="#000066"
         >
+          <IconButton onClick={handleOpenDispatch} size="small" sx={{ position: 'absolute', right: 5 }}>
+            <RiPencilLine />
+          </IconButton>
           <Typography p={1} variant="subtitle1" color="#000066">
             {task.title}
           </Typography>
