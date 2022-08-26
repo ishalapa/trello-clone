e4c476b6cd6786666a81ccd3763dd273edde1a2a
ishalapa <ihor.shalapa@inventorsoft.co>
2022-08-26 09:41:17 +0300

    delete redux persist

--- src/index.js
+++ src/index.js
@@ -7,16 +7,14 @@ import { PersistGate } from 'redux-persist/integration/react';
 
 import './index.css'
 import App from './App'
-import { store, persistor } from 'store'
+import { store } from 'store'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
         <App />
-        </PersistGate>
       </Provider>
     </BrowserRouter>
   </React.StrictMode>
--- src/store/index.js
+++ src/store/index.js
@@ -1,35 +1,17 @@
-import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import { configureStore } from '@reduxjs/toolkit'
 
-import dashboardsReducer from './slices/dashboardsSlice'
-import tasksReducer from './slices/tasksSlice'
-import usersReducer from './slices/usersSlice'
-import descriptionReducer from './slices/descriptionSlice'
-import commentReducer from './slices/commentSlice'
-
-import storage from 'redux-persist/lib/storage'
-import persistReducer from 'redux-persist/es/persistReducer'
-import thunk from 'redux-thunk'
-import persistStore from 'redux-persist/es/persistStore'
-
-const persistConfig = {
-  key: 'root',
-  storage,
-}
-
-const reducers = combineReducers({
-  dashboards: dashboardsReducer,
-  tasks: tasksReducer,
-  users: usersReducer,
-  description: descriptionReducer,
-  comment: commentReducer,
-})
-
-const persistedReducer = persistReducer(persistConfig, reducers)
+import dashboardsReducer from "./slices/dashboardsSlice"
+import tasksReducer from "./slices/tasksSlice"
+import usersReducer from "./slices/usersSlice"
+import descriptionReducer from "./slices/descriptionSlice"
+import commentReducer from "./slices/commentSlice"
 
 export const store = configureStore({
-    reducer: persistedReducer,
-    devTools: process.env.NODE_ENV !== 'production',
-    middleware: [thunk],
-  })
-  
-  export const persistor = persistStore(store)
+    reducer: {
+        dashboards: dashboardsReducer,
+        tasks: tasksReducer,
+        users: usersReducer,
+        description: descriptionReducer,
+        comment: commentReducer
+    }
+})
